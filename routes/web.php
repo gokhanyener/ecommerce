de00b9597fba155a85edf9dd2d8a8d01dd49fdab
Burak Bayram <karubmaryab@gmail.com>
2021-01-16 12:56:19 +0300

    ecommerce initial burak

--- routes/web.php
+++ routes/web.php
@@ -1,46 +1,16 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/test', function () {
-    return view('test');
-});
-
-Route::get('/urun', function () {
-    return ['urun' => 'portakal'];
-})->name('urun');
-
-Route::get('/urun/{urunadi}/{id}', function ($urunadi,$id) {
-    return "Ürün sayısı: $id $urunadi";
-})->name('urunSayisi');
-
-Route::get('/urun/{urunadi}/{id?}', function ($urunadi, $id = 1) {
-    return "Ürün sayısı: $id $urunadi";
-});
-
-Route::get('yonlendir', function () {
-    return redirect()->route('urun');
-});
-
-Route::get('yonlendir2', function () {
-    return redirect()->route('urunSayisi',['urunadi'=>'Muz','id'=>5]);
-});
 
 
-Route::get('page',[\App\Http\Controllers\IndexController::class,'index'])->name('page_route');
-Route::get('show',[\App\Http\Controllers\IndexController::class,'show'])->name('show_route');
+
+Route::get('',[MainController::class,'index'])->name('homepage');
+
+
+Route::view('/category','category')->name('category');
+Route::view('/product','product')->name('product');
+Route::view('/basket','product')->name('basket');
+
+
