01a068fef75c2f9bbb5f4928e98b2af0f61c9471
gökhan <gkhnyener@gmail.com>
2021-01-17 13:04:01 +0300

    controller ve routing düzenlemeleri yapılıyor.

--- app/Http/Controllers/front/MainController.php
+++ app/Http/Controllers/front/MainController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\front;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,17 @@
 <?php
 
-use App\Http\Controllers\MainController;
+use App\Http\Controllers\front\MainController;
+use App\Http\Controllers\front\ProductController;
 use Illuminate\Support\Facades\Route;
 
 
 
 Route::get('', [MainController::class, 'index'])->name('homepage');
+Route::get('/product',[ProductController::class,'index'])->name('product');
+
 
 
 Route::view('/category','front.category')->name('category');
-Route::view('/product/add','front.product')->name('product');
 Route::view('/product/{slug}','front.product-detail')->name('product_detail');
 Route::view('/basket','front.basket')->name('basket');
 Route::view('/login','front.auth.login')->name('login');
