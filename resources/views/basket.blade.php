10473d97905c84803dce97f1b3a97b1ef9a275e9
Burak Bayram <karubmaryab@gmail.com>
2021-01-17 09:32:09 +0300

    master bladesiz şablon yapısı

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -1,1 +1,11 @@
-<?php
+<a href="/">Anasayfa</a>
+<a href="/category">Kategori</a>
+<a href="/product">Ürün</a>
+<a href="/basket">Sepet</a>
+
+<hr>
+Sepet Sayfası
+<hr>
+
+
+E-Ticaret 2021
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,12 +5,22 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>@yield('title',config('app.name'))</title>
 </head>
 <body>
 
-Kategori ismi : {{$kategori}} <br>
-Kategori : {{$isim}}
+<a href="/">Anasayfa</a>
+<a href="/category">Kategori</a>
+<a href="/product">Ürün</a>
+<a href="/basket">Sepet</a>
+
+<hr>
+@yield('content')
+<hr>
+
+
+E-Ticaret 2021
+
 
 </body>
 </html>
