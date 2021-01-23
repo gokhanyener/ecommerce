03d25a2e541b96273724dc436df4b68d61d7241a
gökhan <gkhnyener@gmail.com>
2021-01-23 11:15:35 +0300

    admin panel template oluşturuldu

---
+++ app/Http/Controllers/admin/ProductController.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+class ProductController {
+
+}
---
+++ resources/views/admin/category.blade.php
@@ -0,0 +1,10 @@
+@extends('admin.layouts.master')
+@section('title','Kategoriler')
+@section('content')
+
+    <div class="row">
+        <h1>Kategori sayfası</h1>
+        {{$urun}}
+    </div>
+
+@endsection
