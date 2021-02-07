50891dd939e54d688df4101dd82a2258d4f889ed
gökhan <gkhnyener@gmail.com>
2021-02-07 10:15:41 +0300

    register sayfası hazırlandı.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\ProductDetail;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        $data = $this->categoriesAndOpportunity();
+        //  $categories = $data['categories'];
+        //   $opportunities = $data['opportunities'];
+        //   return view('front.auth.register', compact('categories', 'opportunities'));
+
+        if ($request->isMethod('post')) {
+
+            $user = User::create([
+                'name' =>   Str::of($request->firstName . ' ' . $request->lastName)->title()->trim(),
+              //  'email' => Str::of($request->email)->trim(),
+                'email' => trim($request->email),
+                'password' => Hash::make($request->password)
+            ]);
+
+            auth()->login($user);
+
+            return redirect()->route('homepage');
+        }
+
+        return view('front.auth.register')->with($data);
+    }
+
+    public function categoriesAndOpportunity()
+    {
+
+        $opportunities = ProductDetail::with('product')
+            ->where('opportunity_product', '1')
+            ->take(2)
+            ->get();
+        $categories = Category::with('children')->get();
+
+        return [
+            'opportunities' => $opportunities,
+            'categories' => $categories
+        ];
+    }
+}
